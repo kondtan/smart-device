61f16088652b0d39f4b61e31f0c6018819bedeae
Tanya <kondrashina00@gmail.com>
2021-02-10 15:58:43 +0300

    Оптимизирует изображения

--- build/js/main.js
+++ build/js/main.js
@@ -17,6 +17,24 @@
 
 })();
 
+
+'use strict';
+(function () {
+
+  var inputTelPopup = document.getElementById('feedback-tel-popup');
+  var feedbackTel = document.getElementById('feedback-phone');
+  var maskOptions = {
+    mask: '+{7}(000)000-00-00',
+    lazy: false
+  };
+
+  inputTelPopup.addEventListener('click', function () {
+    var maskPopup = IMask(inputTelPopup, maskOptions);
+    var maskFeedback = IMask(feedbackTel, maskOptions);
+  });
+
+}());
+
 'use strict';
 
 (function () {
