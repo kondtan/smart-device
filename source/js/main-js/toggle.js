53b96c10d7c1ef53e02ad56d0f6c18421eaf36d0
Tanya <kondrashina00@gmail.com>
2021-02-11 11:51:15 +0300

    Поправляет именование в toggle.js, возвращает раскрывашки

--- source/js/main-js/toggle.js
+++ source/js/main-js/toggle.js
@@ -11,26 +11,26 @@
   var contactBlock = document.querySelector('.contact__block');
 
   titleSitemap.addEventListener('click', function () {
-    if (sitemapButton.classList.contains('button-hidden')) {
-      sitemapButton.classList.remove('button-hidden');
-      sitemapButton.classList.add('button-show');
-      sitemapList.classList.toggle('form-show');
+    if (sitemapButton.classList.contains('button--hidden')) {
+      sitemapButton.classList.remove('button--hidden');
+      sitemapButton.classList.add('button--show');
+      sitemapList.classList.toggle('form--show');
     } else {
-      sitemapButton.classList.add('button-hidden');
-      sitemapButton.classList.remove('button-show');
-      sitemapList.classList.toggle('form-show');
+      sitemapButton.classList.add('button--hidden');
+      sitemapButton.classList.remove('button--show');
+      sitemapList.classList.toggle('form--show');
     }
   });
 
   titleContact.addEventListener('click', function () {
-    if (buttonContact.classList.contains('button-hidden')) {
-      buttonContact.classList.remove('button-hidden');
-      buttonContact.classList.add('button-show');
-      contactBlock.classList.toggle('form-show');
+    if (buttonContact.classList.contains('button--hidden')) {
+      buttonContact.classList.remove('button--hidden');
+      buttonContact.classList.add('button--show');
+      contactBlock.classList.toggle('form--show');
     } else {
-      buttonContact.classList.add('button-hidden');
-      buttonContact.classList.remove('button-show');
-      contactBlock.classList.toggle('form-show');
+      buttonContact.classList.add('button--hidden');
+      buttonContact.classList.remove('button--show');
+      contactBlock.classList.toggle('form--show');
     }
   });
 })();
