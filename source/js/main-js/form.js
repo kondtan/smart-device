57efcbd09a694373f0ae8c993c6bee5c7a045888
Tanya <kondrashina00@gmail.com>
2021-02-18 17:26:51 +0300

    Оптимизирует скорость на десктопе

--- source/js/main-js/form.js
+++ source/js/main-js/form.js
@@ -7,10 +7,10 @@
     var userName = form.querySelector('[name=username]');
     var userPhone = form.querySelector('[name=phone]');
     var userText = form.querySelector('[name=feedback-question]');
-    var checkboxPopup = form.querySelector('[name=user-consent]');
+    var consentCheckbox = form.querySelector('[name=user-consent]');
 
     form.addEventListener('submit', function (evt) {
-      if (!userName.value || !userPhone.value || !userText.value || !checkboxPopup.checked) {
+      if (!userName.value || !userPhone.value || !userText.value || !consentCheckbox.checked) {
         evt.preventDefault();
       } else {
         form.submit();
